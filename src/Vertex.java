e1189736d589c3f7fdea7183f5d331e1b86256f2
hibbes <marek@czernhous.de>
2024-09-12 14:41:45 +0200

    Rechnungsübung

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,28 +1,32 @@
 package JWNS26.src;
 
 class Vertex {
-    public double x;
-    public double y;
+  public double x;
+  public double y;
 
-    public Vertex(double x, double y) {
-        this.x = x;
-        this.y = y;
+  public Vertex(double x, double y) {
+    this.x = x;
+    this.y = y;
 
+  }
+
+  public static void main(String[] args) {
+    Vertex v1 = new Vertex(42, 17);
+    Vertex v2 = new Vertex(0.3, 24.6);
+    System.out.println(v1);
+
+    while (v1.x <= 50) {
+
+      v1.x = v1.x + v2.x;
+      v1.y = v1.y + v2.y;
+      if (v1.x <= 50) {
+        System.out.println(v1);
+      }
     }
-    
+  }
 
-    public static void main(String[] args) {
-      Vertex v1 = new Vertex(42,  17);
-      Vertex v2 = new Vertex(0.3,  24.6);
-      System.out.println(v1);
-      v1.x=v1.x+v2.x;
-      v1.y=v1.y+v2.y;
-      System.out.println(v1);
-    }
-
-    public String toString(){
-       return "("+x+","+y+")";
-
-    }
+  public String toString() {
+    return "(" + x + "," + y + ")";
+  }
 
 }
