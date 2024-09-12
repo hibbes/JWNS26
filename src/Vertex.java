072ccaff708fa23afc5e5e8316b093d71181e8d9
hibbes <marek@czernhous.de>
2024-09-12 15:31:17 +0200

    12.09

--- src/Vertex.java
+++ src/Vertex.java
@@ -13,19 +13,32 @@ class Vertex {
   public static void main(String[] args) {
     Vertex v1 = new Vertex(42, 17);
     Vertex v2 = new Vertex(0.3, 24.6);
+    Vertex v3 = v1.skalarMult(2);
+
     System.out.println(v1);
+    System.out.println(v3);
+    System.out.println(v1.length());
+    System.out.println(v2.length());
+   
 
-    while (v1.x <= 50) {
 
-      v1.x = v1.x + v2.x;
-      v1.y = v1.y + v2.y;
-      if (v1.x <= 50) {
-        System.out.println(v1);
-      }
-    }
+  }
+
+  public double length() {
+    return Math.sqrt(x * x + y * y);
+  }
+
+  public Vertex skalarMult(double s) {
+    return new Vertex(x * s, y * s);
+  }
+
+  public void skalarMultMod(double s){
+    x=x*s;
+    y=y*s;
   }
 
   public String toString() {
+
     return "(" + x + "," + y + ")";
   }
 
