d79bea6589b24ca9d9ba3d7ae7bee4400e4065c2
hibbes <marek@czernhous.de>
2024-09-17 08:31:25 +0200

    Weitere Funkionen

--- src/Vertex.java
+++ src/Vertex.java
@@ -19,8 +19,6 @@ class Vertex {
     System.out.println(v3);
     System.out.println(v1.length());
     System.out.println(v2.length());
-   
-
 
   }
 
@@ -32,14 +30,52 @@ class Vertex {
     return new Vertex(x * s, y * s);
   }
 
-  public void skalarMultMod(double s){
-    x=x*s;
-    y=y*s;
+  public void skalarMultMod(double s) {
+    x = x * s;
+    y = y * s;
+  }
+
+  public Vertex add(Vertex v2) {
+    return new Vertex(x + v2.x, y + v2.y);
+  }
+
+  public void addMod(Vertex v2) {
+    x += v2.x;
+    y += v2.y;
+
+    // x=x+v2.x; Langform
+    // y=y+v2.y;
+  }
+
+  public void setX(double x) {
+    this.x = x;
+  }
+
+  public void setY(double y) {
+    this.y = y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
   }
 
   public String toString() {
 
     return "(" + x + "," + y + ")";
   }
+
+ public boolean equals (Object thatObject){
+  if(thatObject instanceof Vertex){
+    that Vertex = (Vertex) thatObject;
+
+    return this.x == that.x && this.y == that.y;
+  }
+  return false;
+
+ }
 
 }
