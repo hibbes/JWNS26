ff4545d3198279a8841b150263e054d208806ca8
hibbes <marek@czernhous.de>
2024-10-01 07:29:20 +0200

    BUgfixes

---
+++ JWNS26/src/FirstPanel.java
@@ -0,0 +1,22 @@
+package JWNS26.src;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FirstPanel extends JPanel {
+    
+    @Override
+    
+    public void paintComponent(java.awt.Graphics g) {
+        g.fillRect(100,100, 200, 200);
+        g.fillRoundRect(400, 100, 100, 50, 10, 10); 
+    }
+
+    public static void main(String[] args){
+            JFrame f = new JFrame();
+            f.add(new FirstPanel());
+           // f.pack();
+            f.setVisible(true);
+        }
+}
+
